c0397ec89369b15b6ad12b01d910a299ed9b38a0
Evelyn Lee <evelynlee34@gmail.com>
2017-10-01 07:54:12 -0400

    Modify server

--- server.js
+++ server.js
@@ -1,56 +1,34 @@
-let express = require('express');
-let mongojs = require('mongojs');
-let app = express();
-let db = mongojs('mongodb', ['custompad']);
-let bodyParser = require('body-parser');
+let express = require(‘express’);
+let mongoose = require(‘mongoose’);
+let bodyParser = require(‘body-parser’);
 
-app.use(express.static(__dirname + "/public"));
+let app = express();
+let router = express.Router();
+let port = process.env.API_PORT || 3000;
+
+//now we should configure the API to use bodyParser and look for
+//JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/charinfo', function (req, res) {
-  console.log("GET request made.");
+//To prevent errors from Cross Origin Resource Sharing, we will set
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
+ res.setHeader(‘Access-Control-Allow-Credentials’, ‘true’);
+ res.setHeader(‘Access-Control-Allow-Methods’, ‘GET,HEAD,OPTIONS,POST,PUT,DELETE’);
+ res.setHeader(‘Access-Control-Allow-Headers’, ‘Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers’);
 
-  db.collection("padsheet").find(function (err, docs) {
-    console.log("Collection obtained:" + docs);
-
-    console.log(res.json(docs));
-  });
+ res.setHeader(‘Cache-Control’, ‘no-cache’);
+ next();
 });
-
-app.post('/charinfo', function (req, res) {
-  console.log(req.body);
-  db.padsheet.insert(req.body, function(err, doc) {
-    res.json(doc);
-  });
+//now we can set the route path & initialize the API
+router.get(‘/’, function(req, res) {
+ res.json({ message: ‘API Initialized!’});
 });
-
-app.delete('/charinfo/:id', function (req, res) {
-  let id = req.params.id;
-  console.log(id);
-  db.padsheet.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  })
+//Use our router configuration when we call /api
+app.use(‘/api’, router);
+//starts the server and listens for requests
+app.listen(port, function() {
+ console.log(`api running on port ${port}`);
 });
-
-app.get('/charinfo/:id', function (req, res) {
-  let id = req.params.id;
-  console.log(id);
-  db.padsheet.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-
-app.put('/charinfo/:id', function (req, res) {
-  let id = req.params.id;
-  console.log(req.body.name);
-  db.padsheet.findAndModify({query: {_id:mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, hp: req.body.hp, atk: req.body.atk, rcv: req.body.rcv}},
-    new: true}, function (err, doc) {
-      res.json(doc);
-  });
-});
-
-let port = 3000 || process.env.PORT;
-
-app.listen(port);
-console.log("Running on " + port.toString());
