fa21017c832d8cc034b2fc4e68a6ca36cfe7d3cb
Evelyn Lee <evelynlee34@gmail.com>
2017-10-08 04:29:05 -0400

    Add mongoose to server

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ let bodyParser = require(‘body-parser’);
 let app = express();
 let router = express.Router();
 let port = process.env.API_PORT || 3000;
+let cors = require('cors');
+
+// Mongoose connection with mongodb
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb', ['custompad'])
+    .then(() => { // if all is ok we will be here
+      console.log('Start');
+    })
+    .catch(err => { // if error we will be here
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
+// Required application specific custom router module
+var charRouter = require('./src/routes/charRouter');
+
+// Use middlewares to set view engine and post json data to the server
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/chars', charRouter);
+
+
 
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
