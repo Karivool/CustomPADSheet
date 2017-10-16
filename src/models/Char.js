d3fab78395c1c6e79eb5a5b2a328bd12f115938c
Evelyn Lee <evelynlee34@gmail.com>
2017-10-16 17:33:13 -0400

    Update model

--- src/models/Char.js
+++ src/models/Char.js
@@ -3,12 +3,20 @@ let Schema = mongoose.Schema;
 
 // Define collection and schema for Chars
   let Char = new Schema({
-    char: {
-      type: String
-    },
+    "no": Number,
+    "name": String,
+    "rare": { Number, default: 1 },
+    "cost": Number,
+    "el1": String, "el2": { String, default: "" },
+    "type1": String, "type2": { String, default: "" }, "type3": { String, default: "" },
+    "hp": { Number, default: 1 }, "atk": { Number, default: 1 }, "rcv": { Number, default: 0 },
+    "leaderttl": { String, default: "" },
+    "leadertxt": { String, default: "" },
+    "activettl": { String, default: "" },
+    "activetxt": { String, default: "" },
+    "cooldown": { Array, default: [1, 1] },
+    "awakenings": { Array, default: [] }
   },
   {
     collection: 'chars'
   });
-
-module.exports = mongoose.model('Char', Char);
