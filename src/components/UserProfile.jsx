855855ed03b97a8acdb9619b9a9a8fe18c744087
Evelyn Lee <evelynlee34@gmail.com>
2017-10-20 23:08:43 -0400

    Add image

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
 // User view bar with signout option
 import React, { Component } from 'react';
+import title from '../title.png'
 
 class UserProfile extends Component {
   render() {
     return (
       <div className="">
-        <h2>CUSTOM PAD SHEET</h2>
+        <img alt="Custom PADSheet" className="title" src={title}/>
       </div>
     )
   }
