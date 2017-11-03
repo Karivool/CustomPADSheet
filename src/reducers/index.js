3462ee1b3715d9fb31e4e12e45d55c2bf9b77868
Evelyn Lee <evelynlee34@gmail.com>
2017-11-03 18:29:58 -0400

    Update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 // Module
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import CharReducer from './char.js';
 
 // File
 
 const rootReducer = combineReducers({
-  chars: CharReducer
+  chars: CharReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
