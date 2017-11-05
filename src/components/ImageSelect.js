0098485240bfc653e948da8f7ce466ddc525bf4e
Evelyn Lee <evelynlee34@gmail.com>
2017-11-05 18:44:37 -0500

    Update ImageSelect

--- src/components/ImageSelect.js
+++ src/components/ImageSelect.js
@@ -3,114 +3,113 @@ import createClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 
-const AWAKENINGS = require('../constants/awakenings');
+const AWAKENINGS = [
+  { value: '', label: 'none', image: '' },
+	{ value: 'skill', label: <img className='dropdown-img' src='/images/awakenings/21.png' alt='SKILL+'></img>},
+	{ value: 'skill-res', label: <img className='dropdown-img' src='/images/awakenings/28.png' alt='SKILLRES'></img>},
+	{ value: 'time', label: <img className='dropdown-img' src='/images/awakenings/19.png' alt='TIME+'></img>},
+	{ value: 'tpa', label: <img className='dropdown-img' src='/images/awakenings/27.png' alt='TPA'></img>},
+	{ value: 'bind-res', label: <img className='dropdown-img' src='/images/awakenings/10.png' alt='BINDRES'></img>},
+	{ value: 'bind-rec', label: <img className='dropdown-img' src='/images/awakenings/20.png' alt='BINDREC'></img>},
+  { value: 'heal', label: <img className='dropdown-img' src='/images/awakenings/9.png' alt='HEAL'></img>},
+  { value: 'blind', label: <img className='dropdown-img' src='/images/awakenings/11.png' alt='BLIND'></img>},
+  { value: 'jammer', label: <img className='dropdown-img' src='/images/awakenings/12.png' alt='JAMMER'></img>},
+  { value: 'poison', label: <img className='dropdown-img' src='/images/awakenings/13.png' alt='POISON'></img>},
+	{ value: 'fire-p', label: <img className='dropdown-img' src='/images/awakenings/14.png' alt='FIRE+'></img>},
+  { value: 'water-p', label: <img className='dropdown-img' src='/images/awakenings/15.png' alt='WATER+'></img>},
+  { value: 'wood-p', label: <img className='dropdown-img' src='/images/awakenings/16.png' alt='WOOD+'></img>},
+  { value: 'light-p', label: <img className='dropdown-img' src='/images/awakenings/17.png' alt='LIGHT+'></img>},
+  { value: 'dark-p', label: <img className='dropdown-img' src='/images/awakenings/18.png' alt='DARK+'></img>},
+	{ value: 'heart-p', label: <img className='dropdown-img' src='/images/awakenings/29.png' alt='HEART+'></img>},
+	{ value: 'fire-r', label: <img className='dropdown-img' src='/images/awakenings/22.png' alt='FIREROW'></img>},
+  { value: 'water-r', label: <img className='dropdown-img' src='/images/awakenings/23.png' alt='WATERROW'></img>},
+  { value: 'wood-r', label: <img className='dropdown-img' src='/images/awakenings/24.png' alt='WOODROW'></img>},
+  { value: 'light-r', label: <img className='dropdown-img' src='/images/awakenings/25.png' alt='LIGHTROW'></img>},
+  { value: 'dark-r', label: <img className='dropdown-img' src='/images/awakenings/26.png' alt='DARKROW'></img>},
+	{ value: 'fire-res', label: <img className='dropdown-img' src='/images/awakenings/4.png' alt='FIRERES'></img>},
+	{ value: 'water-res', label: <img className='dropdown-img' src='/images/awakenings/5.png' alt='WATERRES'></img>},
+	{ value: 'wood-res', label: <img className='dropdown-img' src='/images/awakenings/6.png' alt='WOODRES'></img>},
+	{ value: 'light-res', label: <img className='dropdown-img' src='/images/awakenings/7.png' alt='LIGHTRES'></img>},
+	{ value: 'dark-res', label: <img className='dropdown-img' src='/images/awakenings/8.png' alt='DARKRES'></img>},
+	{ value: 'multi', label: <img className='dropdown-img' src='/images/awakenings/30.png' alt='MULTI+'></img>},
+	{ value: '7combo', label: <img className='dropdown-img' src='/images/awakenings/43.png' alt='7COMBO'></img>},
+	{ value: 'guardbreak', label: <img className='dropdown-img' src='/images/awakenings/44.png' alt='GUARDBREAK'></img>},
+	{ value: 'fua', label: <img className='dropdown-img' src='/images/awakenings/45.png' alt='FUA'></img>},
+	{ value: 'hp', label: <img className='dropdown-img' src='/images/awakenings/1.png' alt='HP'></img>},
+	{ value: 'atk', label: <img className='dropdown-img' src='/images/awakenings/2.png' alt='ATK'></img>},
+	{ value: 'rcv', label: <img className='dropdown-img' src='/images/awakenings/3.png' alt='RCV'></img>},
+	{ value: 'hp-team', label: <img className='dropdown-img' src='/images/awakenings/46.png' alt='HPTEAM'></img>},
+	{ value: 'rcv-team', label: <img className='dropdown-img' src='/images/awakenings/47.png' alt='RCVTEAM'></img>},
+	{ value: '9orb', label: <img className='dropdown-img' src='/images/awakenings/48.png' alt='VOIDSHIELD'></img>},
+  { value: 'dragon-k', label: <img className='dropdown-img' src='/images/awakenings/31.png' alt='DRAGONKILL'></img>},
+  { value: 'god-k', label: <img className='dropdown-img' src='/images/awakenings/32.png' alt='GODKILL'></img>},
+  { value: 'devil-k', label: <img className='dropdown-img' src='/images/awakenings/33.png' alt='DEVILKILL'></img>},
+  { value: 'machine-k', label: <img className='dropdown-img' src='/images/awakenings/34.png' alt='MACHINEKILL'></img>},
+  { value: 'attacker-k', label: <img className='dropdown-img' src='/images/awakenings/35.png' alt='ATTACKERKILL'></img>},
+  { value: 'physical-k', label: <img className='dropdown-img' src='/images/awakenings/36.png' alt='PHYSICALKILL'></img>},
+  { value: 'healer-k', label: <img className='dropdown-img' src='/images/awakenings/37.png' alt='HEALERKILL'></img>},
+  { value: 'balanced-k', label: <img className='dropdown-img' src='/images/awakenings/38.png' alt='BALANCEDKILL'></img>},
+  { value: 'awoken-k', label: <img className='dropdown-img' src='/images/awakenings/39.png' alt='AWOKENKILL'></img>},
+  { value: 'enhance-k', label: <img className='dropdown-img' src='/images/awakenings/40.png' alt='ENHANCEKILL'></img>},
+  { value: 'redeem-k', label: <img className='dropdown-img' src='/images/awakenings/41.png' alt='REDEEMKILL'></img>},
+  { value: 'evo-k', label: <img className='dropdown-img' src='/images/awakenings/42.png' alt='EVOKILL'></img>},
+];
 
-const ImageSelectHandle = createClass({
+var ImageSelect = createClass({
+	displayName: 'ImageSelect',
 	propTypes: {
-		children: PropTypes.node,
-		className: PropTypes.string,
-		isDisabled: PropTypes.bool,
-		isFocused: PropTypes.bool,
-		isSelected: PropTypes.bool,
-		onFocus: PropTypes.func,
-		onSelect: PropTypes.func,
-		option: PropTypes.object.isRequired,
-	},
-	handleMouseDown (event) {
-		event.preventDefault();
-		event.stopPropagation();
-		this.props.onSelect(this.props.option, event);
-	},
-	handleMouseEnter (event) {
-		this.props.onFocus(this.props.option, event);
-	},
-	handleMouseMove (event) {
-		if (this.props.isFocused) return;
-		this.props.onFocus(this.props.option, event);
-	},
-	render () {
-		let imageStyle = {
-			borderRadius: 3,
-			display: 'inline-block',
-			marginRight: 10,
-			position: 'relative',
-			top: -2,
-			verticalAlign: 'middle',
-		};
-		return (
-			<div className={this.props.className}
-				onMouseDown={this.handleMouseDown}
-				onMouseEnter={this.handleMouseEnter}
-				onMouseMove={this.handleMouseMove}
-				title={this.props.option.title}>
-				{this.props.children}
-			</div>
-		);
-	}
-});
-
-const SelectValue = createClass({
-	propTypes: {
-		children: PropTypes.node,
-		placeholder: PropTypes.string,
-		value: PropTypes.object
-	},
-	render () {
-		var imageStyle = {
-			borderRadius: 3,
-			display: 'inline-block',
-			marginRight: 10,
-			position: 'relative',
-			top: -2,
-			verticalAlign: 'middle',
-		};
-		return (
-			<div className="Select-value" title={this.props.value.title}>
-				<span className="Select-value-label">
-				</span>
-			</div>
-		);
-	}
-});
-
-const ImageSelect = createClass({
-	propTypes: {
-		hint: PropTypes.string,
 		label: PropTypes.string,
+		searchable: PropTypes.bool,
+	},
+	getDefaultProps () {
+		return {
+			label: '',
+			searchable: true,
+		};
 	},
 	getInitialState () {
-		return {};
+		return {
+			disabled: false,
+			searchable: this.props.searchable,
+			selectValue: '',
+			clearable: false,
+			rtl: false,
+		};
 	},
-	setValue (value) {
-		this.setState({ value });
+	updateValue (newValue) {
+		debugger
+
+
+		this.setState({
+			selectValue: newValue,
+		});
+	},
+	toggleCheckbox (e) {
+		let newState = {};
+		newState[e.target.name] = e.target.checked;
+		this.setState(newState);
 	},
 	render () {
-		var placeholder = <span>&#9786; Select User</span>;
-
+		var options = AWAKENINGS;
 		return (
 			<div className="section">
-				<h3 className="section-heading">{this.props.label} <a href="https://github.com/JedWatson/react-select/tree/master/examples/src/components/CustomComponents.js">(Source)</a></h3>
 				<Select
-					arrowRenderer={arrowRenderer}
-					onChange={this.setValue}
-					optionComponent={ImageSelectHandle}
-					options={AWAKENINGS}
-					placeholder={placeholder}
-					value={this.state.value}
-					valueComponent={SelectValue}
-					/>
-				<div className="hint">
-					Select option
-				</div>
+					id="state-select"
+					ref="stateSelect"
+					autoFocus
+					options={options}
+					simpleValue
+					clearable={this.state.clearable}
+					name="selected-state"
+					disabled={this.state.disabled}
+					value={this.state.selectValue}
+					onChange={this.updateValue}
+					rtl={this.state.rtl}
+					searchable={this.state.searchable}
+				/>
 			</div>
 		);
 	}
 });
 
-function arrowRenderer () {
-	return (
-		<span>+</span>
-	);
-}
-
-module.exports = ImageSelect;
+export default ImageSelect;
