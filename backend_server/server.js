41ff54d82c0da8d0547938e5570a0130d2d72826
Evelyn Lee <evelynlee34@gmail.com>
2017-11-11 20:49:33 -0500

    Add backend server in seperate folder

---
+++ backend_server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var port = process.env.API_PORT || 4200;
+var cors = require('cors');
+
+// Mongoose connection with mongodb
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://karivool:karivool@address.mlab.com:num/custompad')
+    .then(() => { // if all is ok we will be here
+      console.log('Start');
+    })
+    .catch(err => { // if error we will be here
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
+// Required application specific custom router module
+var charRouter = require('./src/routes/charRoutes');
+
+// Use middlewares to set view engine and post json data to the server
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/chars', charRouter);
+
+// Start the server
+app.listen(port, function(){
+  console.log('Server is running on Port: ',port);
+});
---
+++ backend_server/src/routes/charRoutes.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+var charRouter = express.Router();
+
+// Require Char model in our routes module
+var Char = require('../models/Char');
+
+// Defined store route
+charRouter.route('/add/post').post(function (req, res) {
+  var char = new Char(req.body);
+      char.save()
+    .then(char => {
+    res.status(200).json({Char: 'Char added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+// Defined get data(index or listing) route
+charRouter.route('/').get(function (req, res) {
+  Char.find(function (err, itms){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(itms);
+    }
+  });
+});
+
+// Defined edit route
+charRouter.route('/edit/:id').get(function (req, res) {
+  var id = req.params.id;
+  Char.findById(id, function (err, char){
+      res.json(char);
+  });
+});
+
+//  Defined update route
+charRouter.route('/update/:id').post(function (req, res) {
+  Char.findById(req.params.id, function(err, char) {
+    if (!char)
+      return next(new Error('Could not load Document'));
+    else {
+      // do your updates here
+      char.char = req.body.char;
+
+      char.save().then(char => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+charRouter.route('/delete/:id').get(function (req, res) {
+  Char.findByIdAndRemove({_id: req.params.id},
+	   function(err, char){
+		if(err) res.json(err);
+		else res.json('Successfully removed');
+	});
+});
+
+module.exports = charRouter;
