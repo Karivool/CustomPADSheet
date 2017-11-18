59c0859603a40c50d36fa663e55cad6c0f5d9bf3
Evelyn Lee <evelynlee34@gmail.com>
2017-11-17 22:22:55 -0500

    Update GET and POST reducers

--- src/reducers/char.js
+++ src/reducers/char.js
@@ -2,21 +2,25 @@ import * as Action from '../actions/char_actions.js';
 import merge from 'lodash/merge';
 
 const defaultState = {
-  chars: [],
+  chars: {},
   formOpened: false,
 };
 
 const CharReducer = (state = defaultState, action) => {
   switch(action.type) {
     case Action.ADD_CHAR: {
+      let char = action.info.charData;
+      let updatedChars = state.chars;
+      let updatedData = updatedChars.concat(char);
+      return Object.assign({}, state, { chars: updatedData });
+    }
+    case Action.GET_CHARS: {
+      let chars = action.info.charData;
       debugger
-      let char = action.info.char;
-      let updatedChars = state.char;
-      let updatedData = state.data.concat(action.info.data);
-      return Object.assign({}, state, { char: updatedChars,
-                                        data: updatedData });
+      return Object.assign({}, state, { chars: chars });
     }
     case Action.KILL_CHAR: {
+      debugger
       let idx = action.idx;
 
       let left = state.char.slice(0, idx);
